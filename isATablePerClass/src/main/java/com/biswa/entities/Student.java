fa49a33a5fd1bb7d56aaf007e0e899c081265c4e
Biswaranjan Mohapatra <38851296+bmohapatra1983@users.noreply.github.com>
2019-06-21 13:16:39 +0530

    Student.java

--- isATablePerClass/src/main/java/com/biswa/entities/Student.java
+++ isATablePerClass/src/main/java/com/biswa/entities/Student.java
@@ -3,32 +3,36 @@ package com.biswa.entities;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-
 @Entity
-@Table(name="Student1")
-public class Student extends Person{
+@Table(name = "Student1")
+public class Student extends Person {
 	private String course;
 	private int fee;
+
 	public Student() {
 		super();
 	}
+
 	public Student(String name, String course, int fee) {
 		super(name);
 		this.course = course;
 		this.fee = fee;
 	}
+
 	public String getCourse() {
 		return course;
 	}
+
 	public void setCourse(String course) {
 		this.course = course;
 	}
+
 	public int getFee() {
 		return fee;
 	}
+
 	public void setFee(int fee) {
 		this.fee = fee;
 	}
-	
-	
+
 }
