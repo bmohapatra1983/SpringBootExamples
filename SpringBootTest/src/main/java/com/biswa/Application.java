febe9a8e9dba107e8876494460b68ec9d85341d3
Biswaranjan Mohapatra <38851296+bmohapatra1983@users.noreply.github.com>
2019-09-13 16:36:25 +0530

    Spring Boot Web Application

--- SpringBootTest/src/main/java/com/biswa/Application.java
+++ SpringBootTest/src/main/java/com/biswa/Application.java
@@ -1,20 +1,75 @@
 package com.biswa;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
 @SpringBootApplication
-@ComponentScan({"com.biswa.admin","com.biswa.login"})
-public class Application extends SpringBootServletInitializer {
-	public static void main(String arr[])
-	{
-		//Application is bootstraped
-	  SpringApplication.run(Application.class, arr);
+@ComponentScan({ "com.biswa.admin", "com.biswa.login" })
+public class Application implements CommandLineRunner {
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	public static void main(String arr[]) {
+		// Application is bootstraped
+		SpringApplication.run(Application.class, arr);
 	}
+
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		// TODO Auto-generated method stub
-		return super.configure(builder);
+	public void run(String... args) throws MessagingException, IOException {
+		System.out.println("Sending Email...");
+		sendEmail();
+		// sendEmailWithAttachment();
+		System.out.println("Done");
+	}
+
+	void sendEmail() {
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setTo("bmohapatra1983@gmail.com");
+		msg.setSubject("Testing from Spring Boot");
+		String otp = "878787";
+		msg.setText("Hello World \nSpring Boot Email\nYour OTP is: " + otp);
+		javaMailSender.send(msg);
+	}
+	// ----------------------------
+
+	void sendEmailWithAttachment() throws MessagingException, IOException {
+		MimeMessage msg = javaMailSender.createMimeMessage();
+
+		// true = multipart message
+		MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+		helper.setTo("1@gmail.com");
+
+		helper.setSubject("Testing from Spring Boot");
+
+		// default = text/plain
+		// helper.setText("Check attachment for image!");
+
+		// true = text/html
+		helper.setText("<h1>Check attachment for image!</h1>", true);
+
+		// FileSystemResource file = new FileSystemResource(new
+		// File("classpath:android.png"));
+
+		// Resource resource = new ClassPathResource("android.png");
+		// InputStream input = resource.getInputStream();
+
+		// ResourceUtils.getFile("classpath:android.png");
+
+		helper.addAttachment("my_photo.png", new ClassPathResource("android.png"));
+
+		javaMailSender.send(msg);
+
 	}
 }
--- SpringBootTest/src/main/java/com/biswa/admin/controller/AdminController.java
+++ SpringBootTest/src/main/java/com/biswa/admin/controller/AdminController.java
@@ -1,6 +1,10 @@
 package com.biswa.admin.controller;
 
 import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +13,8 @@ import org.springframework.web.servlet.ModelAndView;
 import com.biswa.admin.bean.User;
 @RestController
 public class AdminController {
+	@Autowired
+	private JavaMailSender javaMailSender;
 	@RequestMapping(value = "/admin", method = RequestMethod.GET)
 	public ArrayList<User> admin() {
 		System.out.println("----Admin-----");
@@ -21,8 +27,19 @@ public class AdminController {
 	}
 	@RequestMapping(value="/hello", method= RequestMethod.GET)
 	public ModelAndView showMessage() {
+		System.out.println("/Hello-----------");
 		ModelAndView mv=new ModelAndView();
 		mv.addObject("abc");
 		return mv;
+	}
+	@RequestMapping(value="/sendMail", method= RequestMethod.GET)
+	void sendEmail() {
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setTo("bmohapatra1983@gmail.com");
+		msg.setSubject("Testing from Spring Boot");
+		String otp = "878787";
+		msg.setText("Hello World \nSpring Boot Email\nYour OTP is: " + otp);
+		javaMailSender.send(msg);
+		System.out.println("Mail Send Successfully.");
 	}
 }
